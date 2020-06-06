6113d0a417bd1873ca638e08878140a788f0293e
Deepjyoti13 <deepjyotide13@gmail.com>
2020-06-07 02:47:14 +0530

    Twentith Update

--- registrationForm/settings.py
+++ registrationForm/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'event',
     'phonenumber_field',
     'admin_app',
+    # 'storages',
 ]
 
 MIDDLEWARE = [
@@ -154,3 +155,11 @@ EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = 'bandeve.dad@gmail.com'
 EMAIL_HOST_PASSWORD = 'bandeve.django'
+
+# AWS_ACCESS_KEY_ID = '*****'
+# AWS_SECRET_ACCESS_KEY = '*/*/*'
+# AWS_STORAGE_BUCKET_NAME = '***'
+# AWS_S3_FILE_OVERWRITE = False
+# AWS_DEFAULT_ACL = None
+# DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+# STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
