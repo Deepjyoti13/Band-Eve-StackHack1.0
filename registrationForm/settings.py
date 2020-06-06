c874c7e4c335ab7343d893359a8dfe19e6ce78a6
Deepjyoti13 <deepjyotide13@gmail.com>
2020-06-07 02:36:30 +0530

    Nineteenth Update

--- registrationForm/settings.py
+++ registrationForm/settings.py
@@ -145,8 +145,8 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
 ]
-MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = 'static/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'static')
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
