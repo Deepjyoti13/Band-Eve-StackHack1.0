6dbbe28dddfa85b2c1f73b72ff25bad0db6f2353
Deepjyoti13 <deepjyotide13@gmail.com>
2020-06-07 00:04:50 +0530

    More Updates

--- registrationForm/settings.py
+++ registrationForm/settings.py
@@ -76,16 +76,16 @@ WSGI_APPLICATION = 'registrationForm.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME':'BandEve',
-        'USER':'postgres',
-        'PASSWORD':'Deepjyoti13.aws',
-        'HOST':'localhost',
-        'PORT':'5432'
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME':'BandEve',
+#         'USER':'postgres',
+#         'PASSWORD':'Deepjyoti13.aws',
+#         'HOST':'localhost',
+#         'PORT':'5432'
+#     }
+# }
 # DATABASES = {
 #     'default': {
 #         'ENGINE': 'django.db.backends.mysql',
@@ -96,6 +96,13 @@ DATABASES = {
 #         'PORT':'3306'
 #     }
 # }
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
