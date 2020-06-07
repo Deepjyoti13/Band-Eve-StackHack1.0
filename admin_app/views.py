47b15ef050cd26af17e69fc477d18234362de107
Deepjyoti13 <deepjyotide13@gmail.com>
2020-06-07 12:40:59 +0530

    Twenty First Update

--- admin_app/views.py
+++ admin_app/views.py
@@ -26,9 +26,6 @@ def admin_page(request):
     return render(request, "admin_page.html", context)
 
 
-# sign in option
-def sign_in_up(request):
-    return render(request, "manager.html")
 
 
 # function to grant access to manager only
@@ -61,56 +58,6 @@ def manager_login(request):
 
     else:
         return render(request, "login.html", {"act": manager_login})
-
-
-# to create new manager account
-def manager_register(request):
-    if request.user.is_authenticated:
-        return redirect("admin_page")
-    # if request is of post type
-    if request.method == "POST":
-
-        # take the necessay fiels in its respective variables
-
-        first_name = request.POST["first_name"]
-        last_name = request.POST["last_name"]
-        username = request.POST["username"]
-        email = request.POST["email"]
-        password1 = request.POST["password1"]
-        password2 = request.POST["password2"]
-
-        # if password and confirm password is equal
-        if password1 == password2:
-            # checks if username is already taken
-            if User.objects.filter(username=username).exists():
-                messages.info(request, "username taken")
-                return redirect("manager_register")
-
-            # checks if the email is already taken
-            elif User.objects.filter(email=email).exists():
-                messages.info(request, "email taken")
-                return redirect("manager_register")
-
-            # create an account and redirect to login page
-            else:
-                user = User.objects.create_user(
-                    username=username,
-                    first_name=first_name,
-                    last_name=last_name,
-                    email=email,
-                    password=password1,
-                )
-                user.save()
-                group = Group.objects.get(name="manager")
-                group.user_set.add(user)
-                return redirect("manager_login")
-
-        else:
-            messages.info(request, "password not matching")
-            return redirect("manager_register")
-
-    else:
-        return render(request, "register.html")
 
 
 # logout
